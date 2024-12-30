fcb445b7d4022bc9e1755c5ee7a8b179fc3caf2a
jun-y23 <ki8109smith@gmail.com>
2024-12-31 08:37:23 +0900

    set up eas

--- libs/supabase.ts
+++ libs/supabase.ts
@@ -3,13 +3,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../types/schema";
 
-const supabaseUrl = process.env.NODE_ENV === "developmentqq"
-	? (process.env.EXPO_PUBLIC_SUPABASE_LOCAL_URL as string) // Use local URL for development
-	: (process.env.EXPO_PUBLIC_SUPABASE_URL as string);
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
 
-const supabaseAnonKey = process.env.NODE_ENV === "developmentqq"
-	? (process.env.EXPO_PUBLIC_SUPABASE_LOCAL_ANON_KEY as string) // Use local anon key for development
-	: (process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string);
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
 	auth: {
